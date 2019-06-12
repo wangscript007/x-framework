bcccb4c9ccdc3cbde36331ffb8d701918e24649f
xust_shinn <495996467@qq.com>
2019-06-12 23:53:50 +0800

    添加result组件

---
+++ src/components/Result/index.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="result-page-wrap">
+    <div class="result-page-icon">
+      <template v-if="$slots.icon"><slot name="icon" /></template>
+      <template v-else>
+        <x-icon
+          :icon="resultIconClass"
+          :class="resultIconColor"
+          class="result-index-icon"
+        />
+      </template>
+    </div>
+    <div class="result-page-index-title">
+      <template v-if="$slots.title"><slot name="title"></slot></template>
+      <template v-else>{{ title }}</template>
+    </div>
+    <div class="result-page-description">
+      <template v-if="$slots.description"><slot name="description"></slot></template>
+      <template v-else>{{ description }}</template>
+    </div>
+    <div class="result-page-extra">
+      <slot name="extra"></slot>
+    </div>
+    <div class="result-page-actions">
+      <slot name="actions"></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Result',
+  props: {
+    /* 类型 'success'|'fail' */
+    type: {
+      type: String,
+      default: 'success'
+    },
+    title: {
+      type: String,
+      default: ''
+    },
+    description: {
+      type: String,
+      default: ''
+    }
+  },
+  computed: {
+    resultIconClass () {
+      return this.type === 'success' ? 'check-circle-fill' : this.type === 'fail' ? 'close-circle-fill' : ''
+    },
+    resultIconColor () {
+      return this.type === 'success' ? 'color-success' : this.type === 'fail' ? 'color-danger' : ''
+    }
+  }
+}
+</script>
